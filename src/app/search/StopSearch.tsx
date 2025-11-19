78b47a7cc280ad4e71e1777984a5570a32890efb
dakShh <daksh.dk207@gmail.com>
2025-11-19 10:31:24 -0500

    cleanup

--- src/app/search/StopSearch.tsx
+++ src/app/search/StopSearch.tsx
@@ -1,35 +1,10 @@
 'use client';
 
-import { Input } from '@/components/ui/input';
-import { useState, useEffect } from 'react';
-
 import AutoComplete from '@/components/custom/autoComplete';
 
 export default function StopSearch() {
-    const [query, setQuery] = useState('');
-    const [results, setResults] = useState([]);
-
-    useEffect(() => {
-        if (query.length < 2) return setResults([]);
-
-        async function searchStops() {
-            const res = await fetch(`/api/stops/search?q=${query}`);
-            const data = await res.json();
-            setResults(data);
-        }
-
-        searchStops();
-    }, [query]);
-
     return (
         <div className="w-full max-w-lg mx-auto">
-            {/* <Input
-                className="w-full p-2 border rounded"
-                placeholder="Search stops (Finch Station, Kennedy...)"
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-            /> */}
-
             <AutoComplete />
         </div>
     );
